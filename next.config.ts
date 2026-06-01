9059bfc5ef6ba37bc7b7b66d6a3f704bccf87651
milgotest32 <milgosutyonetim@gmail.com>
2026-06-01 13:07:04 +0300

    chore: redirectler güncellendi (admin panel)

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,6 @@ const nextConfig: NextConfig = {
     { source: '/collections/tum-urunler', destination: '/urunler', permanent: true },
     { source: '/products/sarimsakli-kekikli', destination: '/urun/sade-surullebilir-peynir', permanent: true },
     { source: '/blogs/tarifler', destination: '/blog', permanent: true },
-    { source: '/blogs/tarifler/milgo-ile-limonlu-cheesecake-tarifi-ferah-hafif-ve-katkisiz-tatli', destination: '/blog/milgo-ile-limonlu-cheesecake-tarifi-ferah-hafif-ve-katkisiz-tatli', permanent: true },
     { source: '/blogs/tarifler/milgo-ile-firinda-patlican-rulo-tarifi', destination: '/blog/milgo-ile-firinda-patlican-rulo-tarifi', permanent: true },
     { source: '/blogs/tarifler/milgo-ile-sebzeli-peynirli-quesadilla-tarifi', destination: '/blog/milgo-ile-sebzeli-peynirli-quesadilla-tarifi', permanent: true },
     { source: '/blogs/tarifler/kremali-makarna', destination: '/blog/kremali-makarna', permanent: true },
@@ -75,6 +74,7 @@ const nextConfig: NextConfig = {
     { source: '/blogs/tarifler/milgo-cig-sut-ile-kakaolu-muhallebi', destination: '/blog', permanent: true },
     { source: '/blogs/tarifler/milgo-cig-sut-ile-besamel-soslu-firin-makarna-tarifi', destination: '/blog', permanent: true },
     { source: '/blogs/tarifler/milgo-cig-sut-ile-kremali-sebze-corbasi', destination: '/blog', permanent: true },
+    { source: '/blogs/tarifler/milgo-ile-limonlu-cheesecake-tarifi-ferah-hafif-ve-katkisiz-tatli', destination: '/blog', permanent: true },
     ]
   },
 }
